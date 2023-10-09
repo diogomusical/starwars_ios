6b98b2eb8515befbe88ba8601e2a5c5d2f74c54e
diogomusical <diogo.musical1@gmail.com>
2023-10-09 14:35:24 +0100

    Initial Commit

---
+++ StarWars_3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StarWars_3
+//
+//  Created by Diogo on 09/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StarWars_3/StarWars_3App.swift
@@ -0,0 +1,17 @@
+//
+//  StarWars_3App.swift
+//  StarWars_3
+//
+//  Created by Diogo on 09/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct StarWars_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
