e028ca8f738c77da8f64c8d163068bbd55f61bf0
Diogo Silva <diogo.coelho-silva@axians.com>
2023-10-19 15:59:11 +0100

    Simplifacação do código Criando a ButtonView e a TextView e correção dos alinhamentos dos diferentes campos dos detalhes das personagens.

---
+++ StarWars_3/ButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonView.swift
+//  StarWars_3
+//
+//  Created by Diogo Coelho da Silva on 19/10/2023.
+//
+
+import SwiftUI
+
+struct ButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonView()
+}
---
+++ StarWars_3/TextView.swift
@@ -0,0 +1,8 @@
+//
+//  TextView.swift
+//  StarWars_3
+//
+//  Created by Diogo Coelho da Silva on 19/10/2023.
+//
+
+import Foundation
